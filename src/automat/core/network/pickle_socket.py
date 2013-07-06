8a5eb75670bf9ea228aa247db790eae84a9fed61
Craig Wm. Versek <cversek+github@gmail.com>
2013-07-06 18:17:45 -0400

    new connection creates new PickleSocket object with its own streams

--- src/automat/core/network/pickle_socket.py
+++ src/automat/core/network/pickle_socket.py
@@ -5,26 +5,38 @@ class UnknownObject(object):
     pass
 
 class PickleSocket:
-    '''recieve and unpickle python objects over the network'''
+    '''receive and unpickle python objects over the network'''
     def __init__(self, sock=None, timeout=None, **kwargs):
         if sock is None:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, **kwargs)
         if timeout is not None:
             sock.settimeout(timeout)
         self.sock = sock
+        #make objects for dumping/loading python objects 
+        self.wfile     = self.sock.makefile('w')
+        self.rfile     = self.sock.makefile('r')
+        self.pickler   = cPickle.Pickler(self.wfile)
+        self.unpickler = cPickle.Unpickler(self.rfile)
     def bind(self,*args,**kwargs):
         self.sock.bind(*args,**kwargs)
+    def get_port(self):
+        hostaddr, port = self.sock.getsockname()
+        return port
     def listen(self,*args,**kwargs):
         self.sock.listen(*args,**kwargs)
     def accept(self, *args,**kwargs):
-        return self.sock.accept(*args,**kwargs)
+        #get a new socket object to handle the connection
+        conn, addr = self.sock.accept(*args,**kwargs)
+        #wrap the connection with a new PickleSocket object
+        conn = PickleSocket(sock=conn)
+        return (conn, addr)
     def connect(self,*args,**kwargs):
         self.sock.connect(*args,**kwargs)
-        #make objects for dumping/loading python objects 
-        self.pickler   = cPickle.Pickler(self.sock.makefile('w'))
-        self.unpickler = cPickle.Unpickler(self.sock.makefile('r'))
+    def close(self):
+        self.sock.close()
     def dump(self,obj):
         self.pickler.dump(obj)
+        self.wfile.flush()
     def load(self):
         try:
             return self.unpickler.load()
